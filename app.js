f66fa14a4cf422b851a7707185fadf836f928f1e
Anjanique Mackey <anjaniquemackey@gmail.com>
2022-06-02 14:00:05 -0700

    Final game for submitting. Will consider adding to this for fun

--- app.js
+++ app.js
@@ -3,19 +3,18 @@ const readline = require("readline-sync")
 let userAction
 const gamer = {
     name: "gamer",
-    inventory: "",
-    hp: 100,
-    isMonsterPresent: false
+    inventory: 0,
+    courage: 100,
+    isMonsterPresent: false,
+    stats() {
+        console.log(`\n${this.name} stats-- \n inventory: ${this.inventory} \n courage: ${this.courage}`)
+    }
 }
 
-// this function returns a random number between 1 and 3
+// this function returns a random number between 1 and 4
 function getrandomNum() {
-    randomNum = Math.floor(Math.random() * 2) + 1
+    randomNum = Math.floor(Math.random() * 3) + 1
     return randomNum
-}
-
-if (gamer.hp <= 0) {
-    console.log(`${gamer.name} has perished. 😢`)
 }
 
 function walk() {
@@ -24,13 +23,15 @@ function walk() {
         encounter()
     }
     while (gamer.isMonsterPresent === false) {
-        userAction = readline.question("...Press W to walk. ").toUpperCase()
+        userAction = readline.question("\n...Press W to walk. or P to check stats ").toUpperCase()
         if (userAction === "W") {
             randomNum = getrandomNum()
             if (randomNum === 1) {
-                gamer.isMonsterPresent === true
+                gamer.isMonsterPresent = true
                 encounter()
             }
+        } else if (userAction === "P") {
+            gamer.stats()
         }
     }
     if (gamer.isMonsterPresent === true) {
@@ -41,7 +42,7 @@ function walk() {
 function encounter() {
     gamer.isMonsterPresent = true;
     enemyObj = getEnemy()
-    userAction = readline.question(`You have encountered ${enemyObj.name}! Fight or run? (F or R) `).toUpperCase()
+    userAction = readline.question(`\nThen, ${gamer.name} encountered ${enemyObj.name}!\n"${enemyObj.phrase}"\nFight or run? (F or R) `).toUpperCase()
     switch (userAction) {
         case "F":
             fight(enemyObj)
@@ -49,11 +50,12 @@ function encounter() {
         case "R":
             const chanceOfRunning = Math.floor(Math.random() * 3)// (0, 2)
             if (chanceOfRunning === 1) {
+                console.log(`${gamer.name} heard ${enemy.name} giggling behind'em "hehehehhehe like I said ${enemyObj.phrase}"`)
                 run()
             } else {
-                console.log(`${gamer.name} failed to escape and took damge!`)
+                console.log(`\n${gamer.name} failed to escape and took dicouragement!`)
                 randomDamage = Math.ceil(Math.random() * 50) + 1
-                gamer.hp -= randomDamage
+                gamer.courage -= randomDamage
                 fight(enemyObj)
             }
     }
@@ -61,24 +63,30 @@ function encounter() {
 
 function run() {
     randomDamage = Math.ceil(Math.random() * 50) + 1
-    gamer.hp -= randomDamage
+    gamer.courage -= randomDamage
     gamer.isMonsterPresent = false
-    console.log(`You took ${randomDamage} points of damage while fleeing! `)
+    console.log(`\${gamer.name} took ${randomDamage} points of discouragement while fleeing! `)
     walk()
 }
 
 const enemy = [
     {
-        name: "enemy1",
-        hp: 50
+        name: "Tree stump with a sign",
+        phrase: "You smell bad.",
+        courage: 30,
+        reward: 1
     },
     {
-        name: "enemy2",
-        hp: 30,
+        name: "Talking Bush",
+        phrase: "You Ugly, fat-smelling fathead!",
+        courage: 30,
+        reward: 1
     },
     {
-        name: "enemy3",
-        hp: 40
+        name: "Darren",
+        phrase: "I will lay you to waste.................................................................................................................................................................",
+        courage: 60,
+        reward: 2
     }
 ]
 // this function returns a random enemy index to the encounter function
@@ -94,41 +102,55 @@ function getDamage() {
     return damage
 }
 
+let isFighting = false
 function fight(enemy) {
-    let enemyHp = enemy.hp
-    let initialDamage = getDamage()
-    gamer.hp -= initialDamage
-    console.log(`${enemy.name} has dealt ${initialDamage} damage!`)
-    while (gamer.hp > 0 || enemyHp > 0) {
-        userAction = readline.question("Press A to attack! ").toUpperCase()
+    isFighting = true
+    let enemyCourage = enemy.courage
+    let damage = getDamage()
+    gamer.courage -= damage
+    console.log(`\n${enemy.name} then dealt ${damage} damage.`)
+    while (isFighting === true) {
+        userAction = readline.question("\nPress A to attack! ").toUpperCase()
         while (!userAction || userAction !== "A") {
-            userAction = readline.question("Press A to attack!").toUpperCase()
+            userAction = readline.question("\nPress A to attack!").toUpperCase()
         }
         if (userAction === "A") {
-            let damageA = getDamage()
-            enemyHp -= damageA
-            console.log(`${gamer.name} dealt ${damageA} damage!`)
-            console.log(`${gamer.name}: ${gamer.hp} hp | ${enemy.name}: ${enemyHp} hp`)
+            if (gamer.courage > 0) {
+                damage = getDamage()
+                enemyCourage -= damage
+                console.log(`\n${gamer.name} dealt ${damage} damage!`)
+                console.log(`${gamer.name}: ${gamer.courage} courage | ${enemy.name}: ${enemyCourage} courage`)
+            }
+            if (enemyCourage > 0) {
+                damage = getDamage()
+                gamer.courage -= damage
+                console.log(`\n${enemy.name} dealt ${gamer.name} ${damage} damage!`)
+            }
+        }
 
-
-            let damageB = getDamage()
-            gamer.hp -= damageB
-            console.log(`${enemy.name} dealt ${gamer.name} ${damageB} damage!`)
+        if (gamer.courage <= 0) {
+            console.log(`\n${gamer.name} became too discouraged and ran home without their pizza. 😢🍽️`)
+            process.exit()
+        }
+        if (enemyCourage <= 0) {
+            gamer.inventory += enemy.reward
+            if (gamer.inventory === 3) {
+                console.log(`\n${gamer.name} collected enough pizza tokens for a free lunch that day. The End 😄🍕🤰`)
+                process.exit()
+            }
+            const awardedCourage = getDamage()
+            console.log(`\n${gamer.name} beat ${enemy.name}, recieved ${awardedCourage} courage, and recieved ${enemy.reward} pizza coupon(s)\n${gamer.name} is walking again.....`)
+            isFighting = !isFighting
+            walk()
         }
     }
-    if (gamer.hp <= 0) {
-        console.log(`${gamer.name} has unfortunately perished. 😢`)
-    }
-    if (enemyHp <= 0) {
-        console.log(`You beat ${enemy.name}!`)
-        walk()
-    }
-
 }
 
-gamer.name = readline.question("You are walking through a dense forest somewhere in the galaxy of Andromeda....erm, what's your name? ")
+// Game play
 
-userAction = readline.question(`Very well, then ${gamer.name}. Press W to walk. `).toUpperCase()
+gamer.name = readline.question("Once upon a time you were walking through the 'Yellow Forest' in the land of Ooo....erm, what's your name? ").toUpperCase()
+
+userAction = readline.question(`\nVery well, then ${gamer.name}. You were on your way for a pizza at Pizza Sally's when you realized this forest is a dangerous one!\nKeep your courage as you trek to your lunch!, and maybe collect 3 pizza coupons from the tree people as you go... Press W to walk. `).toUpperCase()
 
 
 // This will generate a random number and depending on the number will make an enemey appear. and it will run enemy encounter funcion. that will ask if they wanna fight or run. if they runtheres a chance they will not get away.
@@ -139,52 +161,6 @@ if (userAction === "W") {
     } else {
         walk()
     }
+} else if (userAction === "P") {
+    gamer.stats()
 }
-
-
-
-// function randomEvent() {
-//     const num = Math.floor(Math.ciel() * 3)
-//     // event 1
-//     switch (num) {
-//         case "1":
-//             let answer = readline.question("An ugly hippo-dog-rag-face scuttles beside you. Do you want to ask it for money? Y or N? ");
-//             if (answer === "N") {
-
-//             }
-//     }
-// }
-// this function generates a random number and if its 3 monster returns true and it will send them to the encounter function
-// function walk() {
-//     const randomNum = Math.floor(Math.ciel() * 3) // 0, 3
-//     if (randomNum === 0) {
-//         monsterEncounter()
-//     } else {
-//         userAction = readline.question("Nothing yet...... keep walking? Press W. ")
-//     }
-// }
-
-// if (gamer.courage > 0) {
-//     userAction = readline.question("Press W to walk.")
-//     if (userAction === "W") {
-//         walk();
-//     }
-// } else {
-//     console.log(`${gamer.name} has perished. 😢`)
-// }
-
-// gamer.name = readline.question("You are walking through a dense forest somewhere in the galaxy of Andromeda....erm, what's your name? ")
-// userAction = readline.question(`Very well, then ${gamer.name}. Press W to walk. `)
-
-// this function asks tou to walk and if you encounter something sends you to that function
-// function walk() {
-//     userAction = readline.question("Nothing yet...Press W to walk. ")
-//     if (userAction === "W") {
-//         const randomNum = getrandomNum()
-//         if (randomNum === 1) {
-//             encounter()
-//         } else {
-
-//         }
-//     }
-// }
